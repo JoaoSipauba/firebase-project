ea56bd0558b0f44996ffc33d5b04812dc57bb257
JoaoSipauba <joaosipauba@hotmail.com>
2019-12-05 14:31:40 -0300

    componente navbar criado

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import {BrowserRouter as Router, Route } from 'react-router-dom'
 
 // paginas
 import Login from './view/login';
-import NovoUsuario from './view/usuario-novo'
+import NovoUsuario from './view/usuario-novo';
+import Home from './view/home';
 
 function App() {
   return(
     <Router>
-      <Route exact path = '/' component={Login}/>
+      <Route exact path = '/' component={Home}/>
       <Route exact path = '/NovoUsuario' component={NovoUsuario}/>
+      <Route exact path = '/Login' component={Login}/>
     </Router>
   )
 }
